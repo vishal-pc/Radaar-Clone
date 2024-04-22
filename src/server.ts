b890075b652a96e971ea59cb752a417cfda925d4
vishal-pc <vishal.kumar@technocratshorizons.com>
2024-04-22 17:02:56 +0530

    added cluster

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,57 @@
 import app from "./app";
 import http from "http";
+import cluster from "cluster";
+import os from "os";
 import Logger from "./utils/logger";
 import connection from "./config/db.config";
 import { initializeWebSocket } from "./socket/index.socket";
 import { envConfig } from "./config/env.config";
 
-// initialize http server
-const server: http.Server = http.createServer(app);
-const port = envConfig.Port || 3000;
+// Check if the current process is the master process
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  Logger.info(`Master Server Pid ${process.pid} is running...😄`);
 
-// start server and connect db
-function startServer(port: number | undefined) {
-  try {
-    connection
-      .then(() => {
-        Logger.info("Connected to MongoDb DataBase...🔥");
-        server.listen(port, () => {
-          Logger.info(`Connected to Server on http://localhost:${port} ...🚀`);
-          const error = false;
-          if (error) {
-            Logger.error("Unabel to connect Server...😴");
-          }
-        });
-        initializeWebSocket(server);
-      })
-      .catch((err) => {
-        Logger.error("Unable to connect MongoDb...🥱", err);
-      });
-  } catch (error) {
-    Logger.error("Something went wrong...🥲", error);
+  // Fork workers based on the number of CPUs
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-}
 
-startServer(port);
+  // Listen for dying workers and restart them
+  cluster.on("exit", (worker, code, signal) => {
+    Logger.error(`Worker ${worker.process.pid} died`);
+    Logger.info("Restarting worker...");
+    cluster.fork();
+  });
+} else {
+  // This is the worker process, it will act as a server
+  const server: http.Server = http.createServer(app);
+  const port = envConfig.Port || 3000;
+
+  // start server and connect db
+  function startServer(port: number | undefined) {
+    try {
+      connection
+        .then(() => {
+          Logger.info("Connected to MongoDB Database...🔥");
+          server.listen(port, () => {
+            Logger.info(
+              `Connected to Server on http://localhost:${port} ...🚀 and Worker ${process.pid} started...⏳️`
+            );
+            const error = false;
+            if (error) {
+              Logger.error("Unable to connect Server...😴");
+            }
+          });
+          initializeWebSocket(server);
+        })
+        .catch((err) => {
+          Logger.error("Unable to connect MongoDB...🥱", err);
+        });
+    } catch (error) {
+      Logger.error("Something went wrong...🥲", error);
+    }
+  }
+
+  startServer(port);
+}
